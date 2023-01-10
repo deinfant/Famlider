5ecf3e5ec32afcf62b843050e9325e22a8cb292f
deinfant <113513379+deinfant@users.noreply.github.com>
2023-01-10 18:36:32 +0800

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
+const text = document.getElementById("text");
+const texts = ["text 1", "text 2", "text 3", "text 4"];
 
     $(".buddy").on("swiperight",function(){
       $(this).addClass('rotate-left').delay(700).fadeOut(1);
       $('.buddy').find('.status').remove();
-
+      text.innerHTML = texts[Math.floor(Math.random() * texts.length)];
       $(this).append('<div class="status like">Like!</div>');      
       if ( $(this).is(':last-child') ) {
         $('.buddy:nth-child(1)').removeClass ('rotate-left rotate-right').fadeIn(300);
@@ -15,7 +17,7 @@ $(document).ready(function(){
    $(".buddy").on("swipeleft",function(){
     $(this).addClass('rotate-right').delay(700).fadeOut(1);
     $('.buddy').find('.status').remove();
-       
+    text.innerHTML = texts[Math.floor(Math.random() * texts.length)];
     $(this).append('<div class="status dislike">Dislike!</div>');
       if ( $(this).is(':last-child') ) {
         $('.buddy:nth-child(1)').removeClass ('rotate-left rotate-right').fadeIn(300);

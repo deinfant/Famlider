a57f1810af1a85bd17fa09653917be0f0a7c00a2
deinfant <113513379+deinfant@users.noreply.github.com>
2023-01-10 20:43:42 +0800

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 const descc = document.getElementById("textbelowidk");
 const texts = ["Mother: Júlia Vidal, 58.\nFather: Gualter Ferreira, 39. (Uptight)\nSisters (1)\nHayle, 8\nSons (2)\nMateo, 5\nTiburcio, 6 (Deceased)", 
-               "Family Surname: Zapatero-Campo\nGrandmother: Oracion Campo, 52. (On The Run)\nGrandfather: León Zapatero, 62.\nPaternal Aunts\nCielo, 35 (Alcoholic)\nArgentina, 29 (Anti-Social)\nMercado, 26 (Murdered)\nPaternal Uncles\nPatricio, 32 (Very anxious)\nRainerio, 24"
+               "Family Surname: Zapatero-Campo\nGrandmother: Oracion Campo, 52. (On The Run)\nGrandfather: León Zapatero, 62.\nPaternal Aunts\nCielo, 35 (Alcoholic)\nArgentina, 29 (Anti-Social)\nMercado, 26 (Murdered)\nPaternal Uncles\nPatricio, 32 (Very anxious)\nRainerio, 24",
                "Family Surname: Hirama\nMother: Madra Khrebtov, 35. (Religious)\nFather: Sergio Hirama, 29.\nSisters (4)\nNotburga, 14 (Homosexual)\nHeidy, 17 (Depressed)\nEstelle, 11\nRosmaria, 7 (Kidnapped)", 
                "Family Surname: Yoshizawa\nGrandmother: Caroline Obando, 89.\nGrandfather: Patrick Yoshizawa, 95. (Openly Racist)\nPaternal Aunts\nViolet, 65 (Criminal)\nPaternal Uncles\nMorimasa, 66 (Religious)",
                "Family Surname: Donoso\nGrandmother: Constancia Calvo, 53. (Depressed)\nGrandfather: Godofredo Donoso, 60. (Openly Racist)\nMaternal Uncles\nKondo, 27\nVerissimo, 22\nBolade, 35",

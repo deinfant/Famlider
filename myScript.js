09e9b20c3dc47def6a7911a7d09c3828ac5678d6
deinfant <113513379+deinfant@users.noreply.github.com>
2023-01-10 19:49:32 +0800

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -6,6 +6,7 @@ const texts = ["text 1", "text 2", "text 3", "text 4"];
       $(this).addClass('rotate-left').delay(700).fadeOut(1);
       $('.buddy').find('.status').remove();
       descc.textContent = texts[Math.floor(Math.random() * texts.length)];
+      descc.style.textAlign = "center";
       $(this).append('<div class="status like">Like!</div>');      
       if ( $(this).is(':last-child') ) {
         $('.buddy:nth-child(1)').removeClass ('rotate-left rotate-right').fadeIn(300);
@@ -18,6 +19,7 @@ const texts = ["text 1", "text 2", "text 3", "text 4"];
     $(this).addClass('rotate-right').delay(700).fadeOut(1);
     $('.buddy').find('.status').remove();
     descc.textContent = texts[Math.floor(Math.random() * texts.length)];
+    descc.style.textAlign = "center";
     $(this).append('<div class="status dislike">Dislike!</div>');
       if ( $(this).is(':last-child') ) {
         $('.buddy:nth-child(1)').removeClass ('rotate-left rotate-right').fadeIn(300);
